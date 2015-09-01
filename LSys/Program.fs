5e8d17e9f7b6ae3e4e1b23d1f9af5e47e3e6e83b
theor <theo.richart@gmail.com>
2015-09-01 08:51:08 -0400

    cantor dust working with slimDx renderer

--- LSys/Program.fs
+++ LSys/Program.fs
@@ -76,21 +76,22 @@ module LSys =
 //                     rules = function | A -> seq{ yield A; yield B; yield A } | B -> seq { yield B; yield B; yield B } }
 
 open System
-open System.Windows
-open System.Windows.Controls
-open System.Windows.Media
-open System.Windows.Shapes
+//open System.Windows
+//open System.Windows.Controls
+//open System.Windows.Media
+//open System.Windows.Shapes
 
-let runApp f =
-    let app = Application()
-    let w = Window()
-    let canvas = Canvas()//(Width=float 800,Height=float 800)
-    canvas.SetValue(DockPanel.LastChildFillProperty, true)
-    canvas.Background <- Brushes.Beige
-    w.Content <- canvas
-    w.Loaded.Add (fun _ ->f canvas)
-    app.Run(window = w)
+//let runApp f =
+//    let app = Application()
+//    let w = Window()
+//    let canvas = Canvas()//(Width=float 800,Height=float 800)
+//    canvas.SetValue(DockPanel.LastChildFillProperty, true)
+//    canvas.Background <- Brushes.Beige
+//    w.Content <- canvas
+//    w.Loaded.Add (fun _ ->f canvas)
+//    app.Run(window = w)
 
+open System.Drawing
 let test_sys<'a when 'a:equality> (sys:LSys.LSystem<'a>) n = 
     let folder rules axiom i =
         printfn "%i" i
@@ -99,18 +100,87 @@ let test_sys<'a when 'a:equality> (sys:LSys.LSystem<'a>) n =
     let final = [1..n] |> Seq.fold (folder sys.rules) sys.axiom
     final |> Array.map (sprintf "%O")|> printfn "%O"
 //    algea() |> test_sys
-let folder (canvas:Canvas) rules axiom i =
-    let y = (float i)*45.0
-    let w = canvas.ActualWidth / float (Seq.length axiom)
-    axiom |> Seq.iteri (fun j s ->
+let folder rules (axiom,lines) i =
+    let y = (float32 i)*15.0f
+    let w = 800.0f / float32 (Seq.length axiom)
+    let tuples = axiom |> Array.mapi (fun j s ->
         if s = LSys.Cantor.A then
-            let l = Line(X1=w*float j,Y1=y,X2=w*float j+w,Y2=y,Stroke=Brushes.Black,StrokeThickness=40.0)
-            canvas.Children.Add l |> ignore)
-    LSys.step rules axiom
+            let l1 = PointF(w*float32 j,y)
+            let l2 = PointF(w*float32 j+w,y)//,Stroke=Brushes.Black,StrokeThickness=40.0)
+            [|l1;l2|]
+        else [||]) |> Array.collect id
+    (LSys.step rules axiom),(Array.concat [ lines; tuples ])
+
+open System.Windows.Forms
+open SlimDX
+open SlimDX.DXGI
+open SlimDX.Direct3D11
+open SlimDX.Direct2D
+open SlimDX.Windows
 
 [<EntryPoint;STAThread>]
 let main argv = 
+    Application.EnableVisualStyles();
+    Application.SetCompatibleTextRenderingDefault(false);
+    let form = new RenderForm("LSys - Q/A: angleGrowth - W/S: initAngle")
+    let swapChainDescription = new SwapChainDescription(BufferCount = 2,
+                                                        Usage = Usage.RenderTargetOutput,
+                                                        OutputHandle = form.Handle,
+                                                        IsWindowed = true,
+                                                        ModeDescription = new ModeDescription(0, 0, new Rational(60, 1), Format.R8G8B8A8_UNorm),
+                                                        SampleDescription = new SampleDescription(1, 0),
+                                                        Flags = SwapChainFlags.AllowModeSwitch,
+                                                        SwapEffect = SwapEffect.Discard)
+    let (r,device,swapChain) = SlimDX.Direct3D11.Device.CreateWithSwapChain(DriverType.Hardware,
+                                                                            DeviceCreationFlags.BgraSupport,
+                                                                            swapChainDescription)
+    let backBuffer = Surface.FromSwapChain(swapChain, 0)
+    use factory = new SlimDX.Direct2D.Factory()
+    let dpi = factory.DesktopDpi
+    let renderTarget =
+        RenderTarget.FromDXGI(factory,
+                              backBuffer,
+                              new RenderTargetProperties(
+                                  HorizontalDpi = dpi.Width,
+                                  VerticalDpi = dpi.Height,
+                             
+                                  MinimumFeatureLevel = SlimDX.Direct2D.FeatureLevel.Default,
+                                  PixelFormat = new PixelFormat(Format.Unknown, AlphaMode.Ignore),
+                                  Type = RenderTargetType.Default,
+                                  Usage = RenderTargetUsage.None
+                              ));
+    use factory= swapChain.GetParent<SlimDX.DXGI.Factory>()
+    factory.SetWindowAssociation(form.Handle, WindowAssociationFlags.IgnoreAltEnter) |> ignore
+    
+    form.Size = new Size(800, 600) |> ignore
     let sys = LSys.Cantor.system()
+    let _,lines = [1..10] |> Seq.fold (folder sys.rules) (sys.axiom,[||])
+//    let lines = [|Point(0,0); Point(800,100)|]
+
+    let mainLoop () =
+        renderTarget.BeginDraw()
+        renderTarget.Transform <- Matrix3x2.Identity
+        renderTarget.Clear(Color4(Color.Black))
+
+        use brush = new SolidColorBrush(renderTarget, new Color4(Color.White))
+                    
+        for i in 0 .. 2 .. (Array.length lines) - 1 do
+            let a = lines.[i]
+            let b = lines.[i + 1]
+
+            renderTarget.DrawLine(brush, float32 a.X, float32 a.Y, float32 b.X, float32 b.Y, 10.1f)
+
+        renderTarget.EndDraw() |> ignore
+
+        swapChain.Present(0, PresentFlags.None) |> ignore
+
+    MessagePump.Run(form, mainLoop)
+
+    renderTarget.Dispose()
+    swapChain.Dispose()
+    device.Dispose()
+
+
 //    duration (fun () -> test_sys (LSys.Cantor.system()) 14)
-    runApp (fun canvas -> [1..10] |> Seq.fold (folder canvas sys.rules) sys.axiom |> ignore)
-//    0
+//    runApp (fun canvas -> [1..10] |> Seq.fold (folder canvas sys.rules) sys.axiom |> ignore)
+    0
