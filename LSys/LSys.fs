4f7e5fe884b9a420c1a991223859adbf662653b6
theor <theo.richart@gmail.com>
2015-09-08 09:24:49 -0400

    experimentations with quotations

--- LSys/LSys.fs
+++ LSys/LSys.fs
@@ -15,6 +15,8 @@ module LSystem =
         let create axiom rules =
             { axiom = axiom
               rules = Map.ofList rules }
+
+
 let inline (%>) pred succ = (pred, succ)
 let matchRules rules x =
     let matching = rules |> Map.tryFind x
@@ -42,3 +44,52 @@ module PythTree =
     let system() =  LSystem.create [| O |]
                                    [ O %> [|I;LB;O;RB;O|]
                                      I %> [|I;I|] ]
+
+(* ALT IMPL 2 *)
+type Rule2<'a when 'a:equality and 'a:comparison> = (('a -> bool) * Symbol<'a> array)
+type LSystem2<'a when 'a:equality and 'a:comparison> =
+    { axiom: Axiom<'a>
+      rules: Rule2<'a> list }
+module LSystem2 =
+        let create axiom rules =
+            { axiom = axiom
+              rules = rules }
+module PythTree2 =
+    type State = {pos:PointF; angle:int}
+    let init x y = {pos=PointF(x,y); angle=0}
+    type T = O | I | LB | RB
+    let system() =  LSystem2.create [| O |]
+                                   [ (function | O -> true | _ -> false)  %> [|I;LB;O;RB;O|]
+                                     (function | I -> true | _ -> false) %> [|I;I|] ]
+
+(* ALT IMPL 3 *)
+type RuleResult<'a when 'a:equality and 'a:comparison> = Res of Symbol<'a> array
+type Rules3<'a when 'a:equality and 'a:comparison> = ('a -> RuleResult<'a>)
+type LSystem3<'a when 'a:equality and 'a:comparison> =
+    { axiom: Axiom<'a>
+      rules: Rules3<'a> }
+module LSystem3 =
+        let create axiom rules =
+            { axiom = axiom
+              rules = rules }
+module PythTree3 =
+    type State = {pos:PointF; angle:int}
+    let init x y = {pos=PointF(x,y); angle=0}
+    type T = O | I | LB | RB
+    let system() =  LSystem3.create [| O |]
+                                   (function | O -> Res [|I;LB;O;RB;O|]
+                                             | I -> Res [|I;I|]
+                                             | x -> Res [|x|])
+    let f = <@ function | O -> Res [|I;LB;O;RB;O|] | I when 4 = 2 -> Res [||] | I -> Res [|I;I|] | x -> Res [|x|] @>
+    open FSharp.Quotations
+    open FSharp.Quotations.Patterns
+    open FSharp.Quotations.DerivedPatterns
+    open FSharp.Quotations.ExprShape
+    let rec recognizePlus' quotation =
+        match quotation with
+        | UnionCaseTest x -> printfn "UnionCaseTest %A" quotation; ()
+        | ShapeVar v -> ()
+        | ShapeLambda (v,expr) -> recognizePlus' expr
+        | ShapeCombination (o, exprs) -> List.map recognizePlus' exprs |> ignore
+    let an<'a> (f:Quotations.Expr<('a -> 'a[])>) = recognizePlus' f
+    an f
